8c132cc763b92c80f1d50d810fb768c48cd60a24
alish-mayor <alish22061999@gmail.com>
2021-12-10 16:07:50 +0600

    input with title markup done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <v-app>
-    
+    <v-main class="d-flex align-center grey">
+      <v-container class="d-flex justify-center">
+        <v-card class="px-5 py-7 rounded-lg" min-width="300px" width="375px"> 
+          <div class="d-flex justify-flex-start align-center rounded grey lighten-3 pa-2">
+            <box-icon name='search-alt-2' size="sm" color="grey"></box-icon>
+            <input type="text" placeholder="enter the city..." class="ml-2 input">
+          </div>
+          <div class="d-flex align-center justify-space-between mt-3">
+            <h1 class="text-h5 font-weight-medium">Astana, <span class="text-body-1 font-weight-light">Kazakhstan</span></h1>
+            <box-icon name='heart'></box-icon>
+          </div>
+          
+        </v-card>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
@@ -18,3 +32,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+ .input{
+   width: 100%;
+   outline: none;
+ }
+</style>
